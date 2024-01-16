c70220ec91f66b6d02fd4173e796947c65870930
Igor Piedade <igorpiedade@hotmail.com>
2024-01-15 20:16:12 -0500

    feat :sparkles: Save user account to MongoDB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID?? "",
@@ -13,5 +18,9 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET?? "",
     }),
   ],
+  // session: {
+  //   strategy: "jwt",
+  // },
+  // secret: process.env.NEXTAUTH_SECRET,
 });
 export {handler as GET, handler as POST};
